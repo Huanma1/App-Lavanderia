52af3dadbb007af14f5a085c0cd2d0f5660075a1
Juan Orellana <orellanajuan812@gmail.com>
2026-01-25 11:38:25 -0500

    mishx5

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,15 +1,22 @@
+
 #!/bin/bash
+
+# 1. Bajamos a la raíz del proyecto
 cd ..
 
+# 2. Clonamos Flutter solo si no existe la carpeta
 if [ ! -d "flutter" ]; then
-	git clone https://github.com/flutter/flutter.git -b stable --depth 1
+    git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
 
-FLUTTER_PATH=$(pwd)/flutter/bin
-export PATH="$PATH:$FLUTTER_PATH"
+# 3. Configuramos la ruta usando una variable limpia
+export PATH="$PATH:$(pwd)/flutter/bin"
 
+# 4. Forzamos a que Flutter se reconozca y prepare
 flutter precache --ios
 flutter pub get
 
+# 5. Volvemos a la carpeta de iOS e instalamos dependencias
 cd ios
 pod install
+
