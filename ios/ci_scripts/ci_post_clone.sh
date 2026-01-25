5f6b454b776d7d575477125c93d8e6c7a8384eab
Juan Orellana <orellanajuan812@gmail.com>
2026-01-25 11:49:15 -0500

    mishx100

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,22 +1,21 @@
+#!/usr/bin/env bash
 
-#!/bin/bash
+# Salir si hay un error
+set -e
 
-# 1. Bajamos a la raíz del proyecto
-cd ..
+# 1. Instalar Flutter (si no usas una versión específica, puedes clonar el repo)
+git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
 
-# 2. Clonamos Flutter solo si no existe la carpeta
-if [ ! -d "flutter" ]; then
-    git clone https://github.com/flutter/flutter.git -b stable --depth 1
-fi
-
-# 3. Configuramos la ruta usando una variable limpia
-export PATH="$PATH:$(pwd)/flutter/bin"
-
-# 4. Forzamos a que Flutter se reconozca y prepare
+# 2. Pre-descargar artefactos
 flutter precache --ios
+
+# 3. Obtener dependencias
 flutter pub get
 
-# 5. Volvemos a la carpeta de iOS e instalamos dependencias
-cd ios
+# 4. Instalar CocoaPods (fundamental para el error de 'Generated.xcconfig')
+HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
+cd ..
 pod install
 
+exit 0
