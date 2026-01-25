52424ff49d45cd4e3a4334f66c14b92d3837c9e9
Juan Orellana <orellanajuan812@gmail.com>
2026-01-25 11:24:46 -0500

    mishx2

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 cd ..
 
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:'pwd'/flutter/bin"
+if [ ! -d "flutter" ]; then
+	git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+export PATH="$PATH:`pwd`/flutter/bin"
 
 flutter precache --ios
 flutter pub get
